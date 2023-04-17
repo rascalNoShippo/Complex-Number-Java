ef09aefe7a964b9c84b080439a5423a3fb3a04a6
kurosawanao <naofirefox@gmail.com>
2023-04-17 22:39:28 +0900

    add functions

--- src/Complex/Complex.java
+++ src/Complex/Complex.java
@@ -2,28 +2,53 @@ package Complex;
 
 /**
  * 複素数クラス
+ *
  * @author Nao Kurosawa
  */
-public class Complex extends Number{
+public class Complex extends Number {
     private final double re;
     private final double im;
     private static final ArithmeticException ae = new ArithmeticException("/ by zero");
+    public static final Complex ONE = Complex.valueOf(1);
+    public static final Complex I = new Complex(0, 1);
+
+    /**
+     * 実部ゲッター
+     *
+     * @return 実部
+     */
+    public double getRealPart() {
+        return this.re;
+    }
+
+    /**
+     * 虚部ゲッター
+     *
+     * @return 虚部
+     */
+    public double getImaginaryPart() {
+        return this.im;
+    }
 
     public int intValue() {
         return Double.valueOf(this.re).intValue();
     }
+
     public long longValue() {
         return Double.valueOf(this.re).longValue();
     }
+
     public float floatValue() {
         return Double.valueOf(this.re).floatValue();
     }
+
     public double doubleValue() {
         return this.re;
     }
 
     /**
      * 複素数を生成
+     *
      * @param re 実部
      * @param im 虚部
      */
@@ -42,6 +67,7 @@ public class Complex extends Number{
 
     /**
      * 複素数を “a + bi” の形で文字列化
+     *
      * @return 複素数文字列
      */
     @Override
@@ -56,20 +82,45 @@ public class Complex extends Number{
         } else if (!realStr.equals("") && !imagStr.equals("") && this.im > 0) {
             operator = " + ";
         }
-        return (realStr + operator + imagStr).trim().replace(" 1i", " i");
+        return (realStr + operator + imagStr).trim().replace("1i", "i");
     }
 
     /**
      * 数値型からComplexクラスに変換
-     * @param number 数値
+     *
+     * @param realNum 数値
      * @return Complexクラス（虚部は0）
      */
-    public static Complex of(double number) {
-        return new Complex(number, 0);
+    public static Complex valueOf(double realNum) {
+        return new Complex(realNum, 0);
+    }
+
+    /**
+     * 文字列で表された複素数をComplexクラスに変換
+     *
+     * @param complexStr Complexに変換したい文字列
+     * @return Complexクラス
+     * @throws NumberFormatException 変換できない文字列が与えられた場合
+     */
+    public static Complex valueOf(String complexStr) {
+        try {
+            final String trimmedComplexStr = complexStr.trim();
+            final String realPartStr = trimmedComplexStr.replaceFirst("[+-]?\\s?(\\d+(\\.\\d+)?)?i", "")
+                    .replaceAll("\\s", "");
+            final double realPart = realPartStr.isBlank() ? 0 : Double.parseDouble(realPartStr);
+            String imagPartStr = trimmedComplexStr.replaceFirst(realPartStr, "")
+                    .replaceAll("\\s", "");
+            imagPartStr = imagPartStr.replaceFirst("i", imagPartStr.matches("[+-]?i") ? "1" : "");
+            final double imagPart = imagPartStr.isBlank() ? 0 : Double.parseDouble(imagPartStr);
+            return new Complex(realPart, imagPart);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("\"%s\" is not parsable string of complex number.".formatted(complexStr));
+        }
     }
 
     /**
      * 複素数の加算
+     *
      * @param complex 足す数
      * @return 和
      */
@@ -79,6 +130,7 @@ public class Complex extends Number{
 
     /**
      * 複素数の減算
+     *
      * @param complex 引く数
      * @return 差
      */
@@ -88,6 +140,7 @@ public class Complex extends Number{
 
     /**
      * 複素数の乗算
+     *
      * @param complex かける数
      * @return 積
      */
@@ -99,22 +152,34 @@ public class Complex extends Number{
 
     /**
      * 複素数の値が0か
+     *
      * @return 0なら：TRUE
      */
     public boolean isZero() {
-        return this.re == 0 && this.isRealNumber();
+        return this.re == 0 && isReal();
     }
 
     /**
      * 複素数が実数かどうか
+     *
      * @return 実数なら：TRUE
      */
-    public boolean isRealNumber() {
+    public boolean isReal() {
         return this.im == 0;
     }
 
     /**
+     * 複素数が虚数かどうか
+     *
+     * @return 虚数なら：TRUE
+     */
+    public boolean isImaginary() {
+        return !isReal();
+    }
+
+    /**
      * 複素数の除算
+     *
      * @param complex 割る数
      * @return 商
      * @throws ArithmeticException 引数に0が指定された場合
@@ -131,6 +196,7 @@ public class Complex extends Number{
 
     /**
      * 共役複素数を計算
+     *
      * @return 共役複素数
      */
     public Complex conjugate() {
@@ -139,6 +205,7 @@ public class Complex extends Number{
 
     /**
      * 複素数の絶対値を計算
+     *
      * @return 絶対値（double）
      */
     public double abs() {
@@ -147,29 +214,31 @@ public class Complex extends Number{
 
     /**
      * 2つの複素数の値が等しいか
+     *
      * @param obj 比較対象の複素数
      * @return 値が等しければ：TRUE
      */
     @Override
     public boolean equals(Object obj) {
         return obj instanceof Complex && this.re == ((Complex) obj).re && this.im == ((Complex) obj).im;
-
     }
 
     /**
      * 複素数の平方根のうち実部が非負のもの
+     *
      * @return Complex
      */
     public Complex sqrt() {
         final double c = this.re, d = this.im;
         final double constant = c + Math.sqrt(Math.pow(c, 2) + Math.pow(d, 2));
         final double a = Math.sqrt(constant / 2),
-            b = c <= 0 && d == 0 ? Math.sqrt(-c) + 0 : d / Math.sqrt(2 * constant);
+                b = c <= 0 && d == 0 ? Math.sqrt(-c) + 0 : d / Math.sqrt(2 * constant);
         return new Complex(a, b);
     }
 
     /**
      * 複素指数関数 e^z
+     *
      * @return 複素数
      */
     public Complex exp() {
@@ -179,16 +248,18 @@ public class Complex extends Number{
 
     /**
      * 複素数の正弦（サイン）
+     *
      * @return 複素数
      */
     public Complex sin() {
         final Complex A = (new Complex(-this.im, this.re)).exp(), // e^(iz)
-            B = (new Complex(this.im, -this.re)).exp(); //e^(-iz)
+                B = (new Complex(this.im, -this.re)).exp(); //e^(-iz)
         return new Complex((A.im - B.im) / 2, (B.re - A.re) / 2);
     }
 
     /**
      * 複素数の余弦（コサイン）
+     *
      * @return 複素数
      */
     public Complex cos() {
@@ -199,19 +270,21 @@ public class Complex extends Number{
 
     /**
      * 複素数の正接（タンジェント）
+     *
      * @return 複素数
      */
-    public  Complex tan() {
-        return this.sin().dividedBy(this.cos());
+    public Complex tan() {
+        return sin().dividedBy(cos());
     }
 
     /**
      * 複素数の整数乗
+     *
      * @param a 指数
      * @return 複素数
      */
     public Complex pow(int a) {
-        Complex result = Complex.of(1);
+        Complex result = ONE;
         for (int i = 1; i <= Math.abs(a); i++) {
             result = a > 0 ? result.times(this) : result.dividedBy(this);
         }
@@ -220,10 +293,11 @@ public class Complex extends Number{
 
     /**
      * 複素数の偏角（ラジアン）
+     *
      * @return ラジアン：(-π, π]
      */
-    public double compArgRad() {
-        if (this.isZero()) {
+    public double compArg() {
+        if (isZero()) {
             throw ae;
         }
         final double x = this.re, y = this.im;
@@ -234,12 +308,15 @@ public class Complex extends Number{
     }
 
     /**
-     * 複素数の偏角（度）
-     * @return 偏角（度）：(-90, 90]
+     * 複素数の逆数 1/z
+     *
+     * @return 逆数
      */
-    public double compArgDeg() {
-        final double result = this.compArgRad() * 180 / Math.PI;
-        final long rounded = Math.round(result * 100) / 100;
-        return Math.abs(1 -  rounded / result) < Math.pow(2, -50) ? rounded : result;
+    public Complex reciprocal() {
+        return ONE.dividedBy(this);
+    }
+
+    public static Complex polar(double radius, double angRad) {
+        return new Complex(radius * Math.cos(angRad), radius * Math.sin(angRad));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import Complex.Complex;
 
 public class Main {
     public static void main(String[] args) {
-        Complex a = new Complex(0, -1);
-        Complex b = new Complex(1, 2);
-        System.out.println(a.compArgDeg());
-
+        final Complex a = Complex.valueOf("2-i");
+        System.out.println(a);
     }
 }
