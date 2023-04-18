4ccffdc05198eb83c8205d24413e2bc44bcc866b
kurosawanao <naofirefox@gmail.com>
2023-04-18 23:01:59 +0900

    add functions

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import complex.Complex;
 public class Main {
     public static void main(String[] args) {
         final Complex a = new Complex(1, 2);
-        System.out.println(a.plus(a));
+        final var v = Void.TYPE;
+        System.out.println(v);
     }
 }
